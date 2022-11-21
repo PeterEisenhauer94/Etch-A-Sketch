2da9d1b68c75e0aecf84a3cae9b529ed9c45d7e3
Peter Eisenhauer <petereisenhauer94@gmail.com>
2022-11-21 12:28:46 -0500

    add functionality for changing grid size

--- javascript.js
+++ javascript.js
@@ -1,20 +1,22 @@
-//generate 16x16 grid
+//generate grid
 let grid_container = document.querySelector('#grid-container');
+//initial grid size
+let gridSize = 16;
 
-function generateGrid(){
-    for (let i=1; i<=16;i++){
+function generateGrid(gridSize){
+    for (let i=1; i<=gridSize;i++){
         let d = document.createElement('div');
         d.className = 'row';
         d.setAttribute('id', i);
         grid_container.appendChild(d);
-        for (let k=1;k<=16;k++){
+        for (let k=1;k<=gridSize;k++){
             let div = document.createElement('div');
             div.setAttribute('id', k);
             div.className = 'tile';
             d.appendChild(div);
         }
     }
-    
+    changeOnHover()
 }
 
 function reset(){
@@ -23,12 +25,28 @@ function reset(){
     })
 }
 
+function chooseSize(){
+    let input = prompt("Enter a number to generate a square (2 -> 2x2, 3->3x3, etc");
+    gridSize = parseInt(input);
+    destroyGrid();
+    generateGrid(gridSize);
+}
 
-generateGrid();
+function changeOnHover(){
+    document.querySelectorAll('.tile').forEach(function(item){
+        item.addEventListener('mouseover', function(){
+            item.setAttribute('style','background-color: green');
+        })
+    })
+}
+
+function destroyGrid(){
+    grid_container.replaceChildren();
+}
+
+//Generates initial grid
+generateGrid(gridSize);
 
 //make all tiles change color on hover
-document.querySelectorAll('.tile').forEach(function(item){
-    item.addEventListener('mouseover', function(){
-        item.setAttribute('style','background-color: green');
-    })
-})
+
+
