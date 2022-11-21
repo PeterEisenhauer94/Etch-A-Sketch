49606fc83fb8ac2e3c653ba8fedd491191f41875
Peter Eisenhauer <petereisenhauer94@gmail.com>
2022-11-21 04:18:07 -0500

    establish grid generation function

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//generate 16x16 grid
+let grid_container = document.querySelector('#grid-container');
+
+function generateGrid(){
+    for (let i=1; i<=16;i++){
+        let d = document.createElement('div');
+        d.className = 'row';
+        d.setAttribute('id', i);
+        grid_container.appendChild(d);
+        for (let k=1;k<=16;k++){
+            let div = document.createElement('div');
+            div.setAttribute('id', k);
+            div.textContent = 'A';
+            d.appendChild(div);
+        }
+    }
+    
+}
+
+generateGrid();
